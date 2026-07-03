f7c26e5bb19060e750b380f8168165deb55ffe63
Farouq <harbyodunojo@gmail.com>
2026-07-03 15:13:29 +0100

    I've worked on all the weather data, only remaining the weather icon

--- index.js
+++ index.js
@@ -8,11 +8,34 @@ function showData(response) {
 	let humidityDate = document.querySelector("#humidity-data");
 	humidityDate.innerHTML = `${response.data.temperature.humidity}%`;
 	city.innerHTML = response.data.city;
+
+	let date = new Date(response.data.time * 1000);
+	let Days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	let day = Days[date.getDay()];
+	let hour = date.getHours();
+	let minute = date.getMinutes();
+	if (hour < 10) {
+		hour = `0${hour}`;
+	}
+	if (minute < 10) {
+		minute = `0${minute}`;
+	}
+
+	let timeElement = document.querySelector("#time");
+	timeElement.innerHTML = `${day} ${hour}:${minute}`;
 }
 
 function searchCity(city) {
 	let apiKey = "9f1739f0t2608f809957ea4ea4ob5f0b";
-	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 	axios.get(apiUrl).then(showData);
 }
 
@@ -29,26 +52,26 @@ searchForm.addEventListener("submit", submitForm);
 
 searchCity("New York");
 
-let currentDate = new Date();
-let Days = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday",
-];
-let Day = Days[currentDate.getDay()];
-let Hour = currentDate.getHours();
-let Minute = currentDate.getMinutes();
+// let currentDate = new Date();
+// let Days = [
+// 	"Sunday",
+// 	"Monday",
+// 	"Tuesday",
+// 	"Wednesday",
+// 	"Thursday",
+// 	"Friday",
+// 	"Saturday",
+// ];
+// let Day = Days[currentDate.getDay()];
+// let Hour = currentDate.getHours();
+// let Minute = currentDate.getMinutes();
 
-if (Hour < 10) {
-	Hour = `0${Hour}`;
-}
-if (Minute < 10) {
-	Minute = `0${Minute}`;
-}
+// if (Hour < 10) {
+// 	Hour = `0${Hour}`;
+// }
+// if (Minute < 10) {
+// 	Minute = `0${Minute}`;
+// }
 
-let Time = document.querySelector("#time");
-Time.innerHTML = `${Day} ${Hour}:${Minute}`;
+// let Time = document.querySelector("#time");
+// Time.innerHTML = `${Day} ${Hour}:${Minute}`;
