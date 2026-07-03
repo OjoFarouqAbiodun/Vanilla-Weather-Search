8e0b0bab445b71df89fdc4427459e2ac1dc77ea0
Farouq <harbyodunojo@gmail.com>
2026-07-03 13:20:55 +0100

    I rearraged my code to be more readable

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-let searchForm = document.querySelector("#search-form");
+function showData(response) {
+	let temperatureElement = document.querySelector("#temperature");
+	temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+	let weatherCondition = document.querySelector(".weather-condition");
+	weatherCondition.innerHTML = response.data.condition.description;
+	let windDate = document.querySelector("#wind-data");
+	windDate.innerHTML = `${response.data.wind.speed}km/h`;
+	let humidityDate = document.querySelector("#humidity-data");
+	humidityDate.innerHTML = `${response.data.temperature.humidity}%`;
+	city.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+	let apiKey = "9f1739f0t2608f809957ea4ea4ob5f0b";
+	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+	axios.get(apiUrl).then(showData);
+}
 
 function submitForm(event) {
 	event.preventDefault();
 	let searchInput = document.querySelector(".search-form-input");
 	let searchButton = document.querySelector(".search-form-button");
 	let city = document.querySelector("#city");
-	city.innerHTML = searchInput.value;
-
-	let apiKey = "9f1739f0t2608f809957ea4ea4ob5f0b";
-	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=${apiKey}&units=imperial`;
-
-	function show(response) {
-		console.log(response.data);
-		let temperatureElement = document.querySelector("#temperature");
-		temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-		let weatherCondition = document.querySelector(".weather-condition");
-		weatherCondition.innerHTML = response.data.condition.description;
-		let windDate = document.querySelector("#wind-data");
-		windDate.innerHTML = `${response.data.wind.speed}km/h`;
-		let humidityDate = document.querySelector("#humidity-data");
-		humidityDate.innerHTML = `${response.data.temperature.humidity}%`;
-	}
-
-	axios.get(apiUrl).then(show);
+	searchCity(searchInput.value);
 }
 
+let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", submitForm);
+
+searchCity("New York");
 
 let currentDate = new Date();
 let Days = [
